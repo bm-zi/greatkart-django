7b8d43208f3725589f2015554461f4c0ed26232b
bmzi <bmzi@lab.localdomain>
2022-05-18 15:20:35 -0400

    4th commit

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
-from multiprocessing import context
-from tkinter import E
 from django.shortcuts import get_object_or_404, render
+from carts.models import Cart, CartItem
 from .models import Product
 from category.models import Category
 # Create your views here.
+
+from carts.views import _cart_id
 
 def store(request, category_slug=None):
     categories = None
@@ -27,11 +28,18 @@ def store(request, category_slug=None):
 def product_details(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
+        # For test purpose use the two following lines
+        # return HttpResponse(in_cart)
+        # exit()
+
     except Exception as e:
         raise e
         
     context = {
         'single_product': single_product,
+        'in_cart': in_cart
     }    
     return render(request, 'store/product_detail.html', context)
 
